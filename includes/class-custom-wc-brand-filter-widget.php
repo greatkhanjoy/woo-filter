ff900ce0ddc4e11c21d281ff3649a215394cdd67
Imran Hosein Khan Joy <greatkhanjoy@gmail.com>
2023-09-14 07:58:28 +0600

    slight

--- includes/class-custom-wc-brand-filter-widget.php
+++ includes/class-custom-wc-brand-filter-widget.php
@@ -12,7 +12,7 @@ class Custom_WC_Brand_Filter_Widget extends WP_Widget
     {
         $widget_ops = array(
             'classname'   => 'custom_wc_brand_filter_widget',
-            'description' => __('Custom WooCommerce Brand Filter Widget', 'browter-woofilter'),
+            'description' => __('Browter WooCommerce Brand Filter Widget', 'browter-woofilter'),
         );
         parent::__construct('custom_wc_brand_filter_widget', __('Brand Filter', 'browter-woofilter'), $widget_ops);
     }
@@ -25,33 +25,63 @@ class Custom_WC_Brand_Filter_Widget extends WP_Widget
      */
     public function widget($args, $instance)
     {
-        if (!is_shop() && !is_product_taxonomy()) {
-            return;
-        }
-
+        $title = !empty($instance['title']) ? $instance['title'] : __('Brands', 'browter-woofilter');
         $taxonomy = 'product_brand';
         $terms = get_terms($taxonomy, array(
             'hide_empty' => true,
+            'orderby' => 'name',
+            'order' => 'ASC',
         ));
 
         echo $args['before_widget'];
 
-        if (!empty($terms)) {
-            echo $args['before_title'] . apply_filters('widget_title', __('Brands', 'browter-woofilter')) . $args['after_title'];
-            echo '<ul>';
-
-            foreach ($terms as $term) {
-                $term_link = get_term_link($term, $taxonomy);
-                if (is_wp_error($term_link)) {
-                    continue;
-                }
-
-                echo '<li><label><input type="checkbox" class="filter-checkbox" value="' . esc_attr($term->slug) . '"> ' . $term->name . '</label></li>';
-            }
-
-            echo '</ul>';
+        if ($title) {
+            echo $args['before_title'] . apply_filters('widget_title', $title) . $args['after_title'];
         }
 
+        echo '<ul>';
+
+        foreach ($terms as $term) {
+            $term_link = get_term_link($term, $taxonomy);
+
+            if (is_wp_error($term_link)) {
+                continue;
+            }
+
+            echo '<li><label><input type="checkbox" class="filter-checkbox" value="' . esc_attr($term->slug) . '"> ' . $term->name . '</label></li>';
+        }
+
+        echo '</ul>';
         echo $args['after_widget'];
+    }
+
+    /**
+     * Output the widget options form on the admin side
+     *
+     * @param array $instance
+     */
+    public function form($instance)
+    {
+        $title = !empty($instance['title']) ? $instance['title'] : '';
+?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'browter-woofilter'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
+        </p>
+<?php
+    }
+
+    /**
+     * Update the widget options
+     *
+     * @param array $new_instance
+     * @param array $old_instance
+     * @return array
+     */
+    public function update($new_instance, $old_instance)
+    {
+        $instance = array();
+        $instance['title'] = !empty($new_instance['title']) ? sanitize_text_field($new_instance['title']) : '';
+        return $instance;
     }
 }
